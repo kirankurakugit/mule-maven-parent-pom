dd18cf5e942bd01cac752c0fdad44dd959c4beb9
kirankurakugit <42097142+kirankurakugit@users.noreply.github.com>
2020-06-18 23:53:32 +0530

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -16,7 +16,7 @@ import javax.mail.internet.*
 public static void send(String addresser, String subject, String to, String cc, String bcc, String content, String mimeType) {
 	// Init constants of sender email account.
 	String email = "kiran.kurakula@cloudvisory.com"
-	String password = "Ni$hal12"
+	String password = 'Ni$hal12'
 	String host = "smtp.office365.com" // e.g. -> "smtp.google.com"
 	String port = "587" // e.g. -> "465" "587"
 
