0b714a9707c0629ccdc247df6e86ef6296439e56
kirankurakugit <42097142+kirankurakugit@users.noreply.github.com>
2020-06-18 19:11:44 +0530

    Update smtp.groovy

--- smtp.groovy
+++ smtp.groovy
@@ -4,8 +4,8 @@
 import javax.mail.*
 import javax.mail.internet.*
 
-MAILER_HOST = "aspmx.l.google.com"  // "smtp-relay.gmail.com"
-RECIPIENT_EMAIL = "my+test@example.com"
+MAILER_HOST = "smtp.office365.com"  // "smtp-relay.gmail.com"
+RECIPIENT_EMAIL = "kiran.kurakula@fireeye.com"
 date_time = new Date().format("yyyy-MM-dd hh:mm")
 props = new Properties()
 
@@ -15,7 +15,7 @@ private void runScript() {
     session.setDebug(true);
 
     MimeMessage message = new MimeMessage(session)
-    message.setFrom("no.reply@example.org")
+    message.setFrom("kiran.kurakula@cloudvisory.com")
     message.setRecipient(Message.RecipientType.TO, new InternetAddress(RECIPIENT_EMAIL))
     message.setSubject("A Test email ${date_time}")
     message.setText("This is a sample email message!")
